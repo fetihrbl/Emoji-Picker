8787556b5554c588f72d6a87013698fb8034331a
Fatih <fatihrbl@gmail.com>
2025-04-09 18:39:31 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import EmojiPicker from 'emoji-picker-react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
+  const [chosenEmoji, setChosenEmoji] = useState('');
+
+  const copyToClipboard = async (emoji) => {
+    try{
+      await navigator.clipboard.writeText(emoji);
+      toast.success('Emoji Copied', {
+        position: 'top-center',
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+      })
+    } catch {
+      toast.error('Copy Failed');
+    }
+  }
+
+  const handleEmojiClick = (emojiData) => {
+    const emoji = emojiData.emoji;
+    setChosenEmoji(emoji);
+    copyToClipboard(emoji);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="emoji-app">
+        <h1>Chose and Copy Emoji</h1>
+        {chosenEmoji && (
+          <div className="selected-emoji-card">
+          <p>Chosen Emoji:</p>
+          <div className="emoji-preview">{chosenEmoji}</div>
+        </div>
+        )}
+         <div className="picker-container">
+        <EmojiPicker onEmojiClick={handleEmojiClick} />
+      </div>
+      <ToastContainer />
     </div>
   );
 }
